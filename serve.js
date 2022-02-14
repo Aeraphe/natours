8ccd7ed8b9e7a81c9a3d74d452cb6f09c9ecfbef
Aeraphe <alberto.aeraph@gmail.com>
2022-02-14 18:57:31 -0300

    fix(heroku): fiz get method on express serve

--- serve.js
+++ serve.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist/valeverde'));
 
-app.get('/+',(req,resp)=>{
+app.get('/*',(req,resp)=>{
 
     resp.sendFile(__dirname + '/dist/valeverde/index.html')
 
